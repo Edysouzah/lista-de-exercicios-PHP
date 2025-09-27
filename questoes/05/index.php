667cea4bcce9e8fa20a44c3f72f1062d350802d4
Edysouzah <158646916+Edysouzah@users.noreply.github.com>
2025-09-26 21:02:07 -0300

    Update index.php

--- questoes/05/index.php
+++ questoes/05/index.php
@@ -16,7 +16,26 @@
 
     <main>
 
+    <main>
     <!-- implementação da solução -->
+
+        $n1 = 10;
+        $n2 = 25;
+        $n3 = 15;
+
+        $maior = $n1;
+
+        if ($n2 > $maior) {
+            $maior = $n2;
+        }
+
+        if ($n3 > $maior) {
+            $maior = $n3;
+        }
+
+        echo "<p>O maior número é: $maior</p>";
+    
+        
      
     </main>
 </body>
